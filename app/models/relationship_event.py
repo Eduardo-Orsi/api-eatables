97549b9ea189bea9642de52c3e8a1076a3736293
Eduardo Orsi <duduayr@gmail.com>
2024-11-07 14:04:35 -0300

    missing file

--- app/models/relationship_event.py
+++ app/models/relationship_event.py
@@ -1,37 +1,33 @@
 import re
 import enum
 import uuid
-import base64
 
 from unidecode import unidecode
-from sqlalchemy import Column, String, Date, Time, Enum
+from sqlalchemy import Column, String, Date, Time, Enum, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
 from ..db.database import Base
+from ..utils.generate_small_id import generate_small_id
 
 
 class PlanType(enum.Enum):
     SIMPLE = "simple_plan"
     ADVANCED = "advanced_plan"
 
-
-def generate_short_id() -> str:
-    unique_id = base64.urlsafe_b64encode(uuid.uuid4().bytes).decode('utf-8').rstrip("=")
-    return unique_id[:8]
-
-
 class RelationshipEvent(Base):
     __tablename__ = 'relationship_events'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    small_id = Column(String(8), unique=True, nullable=False, default=generate_short_id)
+    small_id = Column(String(8), unique=True, nullable=False, default=generate_small_id)
+    email = Column(String, nullable=False)
     couple_name = Column(String, nullable=False)
     couple_slug = Column(String, nullable=False)
     relationship_beginning_date = Column(Date, nullable=False)
     relationship_beginning_hour = Column(Time, nullable=False)
     message = Column(String, nullable=False)
     plan = Column(Enum(PlanType), nullable=False)
+    paid = Column(Boolean, default=False)
 
     files = relationship('File', back_populates='relationship_event')
 
--- app/schema/relationship_event_schema.py
+++ app/schema/relationship_event_schema.py
@@ -1,10 +1,19 @@
+from enum import Enum
 from datetime import date, time
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
+
+
+class PlanType(Enum):
+    SIMPLE = "simple_plan"
+    ADVANCED = "advanced_plan"
 
 
 class RelationshipEventForm(BaseModel):
+    email: EmailStr = Field(..., description="E-mail do cliente")
     couple_name: str = Field(..., description="Nome do casal", max_length=255, min_length=3)
     relationship_beginning_date: date
     relationship_beginning_hour: time
     message: str = Field(..., description="Mensagem surpresa para o mozão", max_length=255)
+    plan: PlanType = Field(..., description="Tipo de plano escolhido, 'simple_plan' ou 'advanced_plan'")
+    paid: bool = Field(default=False, description="O cliente pagou o site ou não")
