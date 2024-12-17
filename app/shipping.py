0f75f8c52b219820524a620b7c14414ffed55767
Eduardo Orsi <duduayr@gmail.com>
2024-12-17 17:00:05 -0300

    s,ndlnsldsnd

--- app/shipping.py
+++ app/shipping.py
@@ -185,9 +185,7 @@ async def register_email(email: EmailRegsiter, db: Session = Depends(get_db)):
         love_cards_customer = LoveCardsCustomer(email=email.email)
         db.add(love_cards_customer)
         db.commit()
-        return Response({
-            "message": "E-mail adicionado com sucesso!"
-        }, status_code=200)
+        return Response("E-mail adicionado com sucesso!", status_code=200)
 
     except IntegrityError as ex:
         raise HTTPException(status_code=400, detail="E-mail já está cadastrado") from ex
